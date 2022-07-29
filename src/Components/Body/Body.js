566fefa503bf414ace202e8e86a423b8d9cc6bae
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-29 23:45:12 +0530

    Editor Categories can done

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import styles from './Body.module.css'
 import { ArrowDown } from 'react-feather'
+import { Editor } from '../../Editor/Editor'
 
 export const Body = () => {
 
-    const colors = ['#239ce2', '#48bb78', '#0bc5ea', '#a0a3c0', '#ed8936']
+    const colors = ['#239ce2', '#48bb78', '#0bc5ea', '#a0a3c0', '#ed8936'];
+    const sections = {
+        basicInfo : 'Basic Info',
+        workExp : 'Work Experience',
+        projects : 'Projects',
+        education : 'Education',
+        achievments : 'Achievments',
+        summary : 'Summary',
+        other : 'Other'
+    }
   return (
     <div className={styles.container}>
         <p className={styles.heading}>Build Resume</p>
@@ -22,6 +32,9 @@ export const Body = () => {
                 <span className={styles.color}></span>
             </div>
             <button><ArrowDown/>Download</button>
+        </div>
+        <div className={styles.main}>
+            <Editor sections = {sections}/>
         </div>
     </div>
   )
--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./Editor.module.css";
 
-export const Editor = () => {
+export const Editor = ({ sections }) => {
+  const [activeSectionKey, setActiveSectionKey] = useState(
+    Object.keys(sections)[0]
+  );
   return (
-    <div>Editor</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.header}>
+        {Object.keys(sections).map((key) => (
+          <div
+            className={`${styles.section} ${
+              activeSectionKey === key ? styles.active : ""
+            }`}
+            key={key}
+            onClick={() => setActiveSectionKey(key)}
+          >
+            {sections[key]}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.body}>
+        
+      </div>
+    </div>
+  );
+};
