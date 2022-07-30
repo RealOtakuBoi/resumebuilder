1559f92d9254ab66365c5eedb9a924b9d156f16e
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-30 22:34:12 +0530

    Some Functionalities

--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -16,17 +16,40 @@ export const Editor = ({ sections, information }) => {
     sections[Object.keys(sections)[0]]
   )
 
+  const [values, setValues] = useState({
+    name: activeInformation?.detail?.name || '',
+    title: activeInformation?.detail?.title || '',
+    linkedin: activeInformation?.detail?.linkedin || '',
+    github: activeInformation?.detail?.github || '',
+    phone: activeInformation?.detail?.phone || '',
+    email: activeInformation?.detail?.email || '',
+  })
+
+  const handlePointUpdate = (value, index) => {
+    const tempValues = {...values}
+    tempValues.points[index]=value
+    setValues(tempValues)
+  }
+
   const workExpBody = (
     <div className={styles.detail}>
       <div className={styles.row}>
         <InputControl
           label="Title"
-          placeholder="Enter title eg. Frontend Developer"
+          placeholder="Enter Title eg. Frontend Developer"
+          defaultValue = {values.title}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
           
         />
         <InputControl
           label="Company Name"
-          placeholder="Enter company name eg. Amazon"
+          placeholder="Enter Name of a Company"
+          defaultValue = {values.companyName}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, companyName: event.target.value }))
+          }
           
         />
       </div>
@@ -34,11 +57,19 @@ export const Editor = ({ sections, information }) => {
         <InputControl
           label="Certificate Link"
           placeholder="Enter certificate link"
+          defaultValue = {values.certificateLink}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, certificateLink: event.target.value }))
+          }
          
         />
         <InputControl
           label="Location"
-          placeholder="Enter Location eg. Remote"
+          placeholder="Enter Location"
+          defaultValue = {values.location}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, location: event.target.value }))
+          }
           
         />
       </div>
@@ -47,11 +78,19 @@ export const Editor = ({ sections, information }) => {
           label="Start Date"
           type="date"
           placeholder="Enter start date of work"
+          defaultValue = {values.startDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, startDate: event.target.value }))
+          }
           
         />
         <InputControl
           label="End Date"
           type="date"
+          defaultValue = {values.endDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, endDate: event.target.value }))
+          }
           
         />
       </div>
@@ -60,15 +99,18 @@ export const Editor = ({ sections, information }) => {
         <label>Enter work description</label>
         <InputControl
           placeholder="Line 1"
-          
+          defaultValue = {values.points ? values.points[0] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 0)}
         />
         <InputControl
           placeholder="Line 2"
-          
+          defaultValue = {values.points ? values.points[1] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 1)}
         />
         <InputControl
           placeholder="Line 3"
-          
+          defaultValue = {values.points ? values.points[2] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 2)}
         />
       </div>
     </div>
@@ -79,35 +121,60 @@ export const Editor = ({ sections, information }) => {
         <InputControl
           label="Title"
           placeholder="Enter Project Name"
+          defaultValue = {values.title}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
+          
         />
       </div>
       <InputControl
         label="Overview"
         placeholder="Enter basic overview of project"
+        defaultValue = {values.overview}
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, overview: event.target.value }))
+        }
       />
       <div className={styles.row}>
         <InputControl
           label="Deployed Link"
           placeholder="Enter deployed link of project"
+          defaultValue = {values.link}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, link: event.target.value }))
+          }
         />
         <InputControl
           label="Github Link"
-          placeholder="Enter github link of project"
+          placeholder="Enter Github Repo link of project"
+          defaultValue = {values.github}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, github: event.target.value }))
+          }
         />
       </div>
       <div className={styles.column}>
         <label>Enter project description</label>
         <InputControl
           placeholder="Line 1"
+          defaultValue = {values.points ? values.points[0] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 0)}
         />
         <InputControl
           placeholder="Line 2"
+          defaultValue = {values.points ? values.points[1] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 1)}
         />
         <InputControl
           placeholder="Line 3"
+          defaultValue = {values.points ? values.points[2] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 2)}
         />
         <InputControl
           placeholder="Line 4"
+          defaultValue = {values.points ? values.points[3] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 3)}
         />
       </div>
     </div>
@@ -117,24 +184,42 @@ export const Editor = ({ sections, information }) => {
       <div className={styles.row}>
         <InputControl
           label="Title"
-          placeholder="Enter title eg. B-tech"
+          placeholder="Enter Title eg. B-tech"
+          defaultValue = {values.title}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
+          
+
           
         />
       </div>
       <InputControl
         label="College/School Name"
         placeholder="Enter name of your College/School"
+        defaultValue = {values.collage}
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, collage: event.target.value }))
+        }
       />
       <div className={styles.row}>
         <InputControl
           label="Start Date"
           type="date"
           placeholder="Enter start date of the education"
+          defaultValue = {values.startDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, startDate: event.target.value }))
+          }
         />
         <InputControl
           label="End Date"
           type="date"
           placeholder="Enter end date of the education"
+          defaultValue = {values.endDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, endDate: event.target.value }))
+          }
         />
       </div>
     </div>
@@ -145,29 +230,53 @@ export const Editor = ({ sections, information }) => {
         <InputControl
           label="Name"
           placeholder="Enter your Full Name"
+          defaultValue = {values.name}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
         />
         <InputControl
           label="Title"
+          defaultValue = {values.title}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
         />
       </div>
       <div className={styles.row}>
         <InputControl
           label="Linkedin Link"
           placeholder="Enter your Linkedin Profile link"
+          defaultValue = {values.linkedin}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, linkedin: event.target.value }))
+          }
         />
         <InputControl
           label="Github Link"
           placeholder="Enter your Github Profile link"
+          defaultValue = {values.github}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, github: event.target.value }))
+          }
         />
       </div>
       <div className={styles.row}>
         <InputControl
           label="Email"
           placeholder="Enter your Email"
+          defaultValue = {values.email}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
         />
         <InputControl
           label="Enter phone"
           placeholder="Enter your Phone Number"
+          defaultValue = {values.phone}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, phone: event.target.value }))
+          }
         />
       </div>
     </div>
@@ -178,15 +287,23 @@ export const Editor = ({ sections, information }) => {
         <label>List your achievements</label>
         <InputControl
           placeholder="Line 1"
+          defaultValue = {values.points ? values.points[0] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 0)}
         />
         <InputControl
           placeholder="Line 2"
+          defaultValue = {values.points ? values.points[1] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 1)}
         />
         <InputControl
           placeholder="Line 3"
+          defaultValue = {values.points ? values.points[2] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 2)}
         />
         <InputControl
           placeholder="Line 4"
+          defaultValue = {values.points ? values.points[3] : ''}
+          onChange={(event) => handlePointUpdate(event.target.value, 3)}
         />
       </div>
     </div>
@@ -196,6 +313,10 @@ export const Editor = ({ sections, information }) => {
       <InputControl
         label="Summary"
         placeholder="Enter your objective/summary"
+        defaultValue = {values.summary}
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, summary: event.target.value }))
+        }
       />
     </div>
   );
@@ -204,6 +325,10 @@ export const Editor = ({ sections, information }) => {
       <InputControl
         label="Other"
         placeholder="Enter some Text"
+        defaultValue = {values.other}
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, other: event.target.value }))
+        }
       />
     </div>
   );
