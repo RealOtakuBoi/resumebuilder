88d29c1b648a20f017275eb78af9e852f3f3505a
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-31 18:32:43 +0530

    dummy done

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,90 +1,155 @@
+import React, { useEffect, useState } from "react";
 
+import Styles from "./Resume.module.css";
 
-import React, { useEffect, useState } from 'react'
-
-import Styles from './Resume.module.css'
-
-import { GitHub } from 'react-feather'
-import { Linkedin } from 'react-feather'
-import { AtSign } from 'react-feather'
-import { Phone } from 'react-feather'
+import { Calendar, GitHub, MapPin } from "react-feather";
+import { Linkedin } from "react-feather";
+import { AtSign } from "react-feather";
+import { Phone } from "react-feather";
+import { Paperclip } from "react-feather";
 
 function Resume(props) {
+  const [column, setColumn] = useState([[], []]);
 
-    const [column, setColumn] = useState([[],[]]);
-
-    const workExpSection = (<div key={'workExp'} className={`${Styles.section} ${Styles.workExp}`}>
-        <div className={Styles.sectionTitle}>Work Experience</div>
-    </div>)
-
-const projectsSection = (<div key={'projects'} className={`${Styles.section} ${Styles.projects}`}>
-<div className={Styles.sectionTitle}>Projects</div>
-</div>)
-
-const educationSection = (<div  key={'education'} className={`${Styles.section} ${Styles.education}`}>
-        <div className={Styles.sectionTitle}>Education</div>
-    </div>)
-
-const achievmentsSection = (<div key={'achievments'} className={`${Styles.section} ${Styles.achievments}`}>
-<div className={Styles.sectionTitle}>Achievments</div>
-</div>)
-
-const otherSection = (<div key={'other'} className={`${Styles.section} ${Styles.other}`}>
-<div className={Styles.sectionTitle}>Other</div>
-</div>)
-
-const summarySection = (<div  key={'summary'} className={`${Styles.section} ${Styles.summary}`}>
-<div className={Styles.sectionTitle}>Summary</div>
-</div>)
-
-
-    useEffect(() => {
-        setColumn([
-            [
-                projectsSection, educationSection, summarySection
-            ],
-
-            [
-                workExpSection, achievmentsSection, otherSection
-            ]
-        ])
-    },[])
-
-    
-  return (
-
-    <div className={Styles.container}>
-        <div className={Styles.header}>
-            <p className={Styles.heading}>Name</p>
-            <p className={Styles.subHeading}>Blockchain Developer</p>
-
-            <div className={Styles.links}>
-                <a className={Styles.link}>
-                    <AtSign/>
-                    abc@gmail.com</a>
-                <a className={Styles.link}>
-                    <Linkedin/>
-                    linkedin.com</a>
-                <a className={Styles.link}>
-                    <GitHub/>
-                    github.com</a>
-
-                <a className={Styles.link}>
-                    <Phone/>
-                    1234567890</a>
-
-            </div>
+  const workExpSection = (
+    <div key={"workExp"} className={`${Styles.section} ${Styles.workExp}`}>
+      <div className={Styles.sectionTitle}>Work Experience</div>
+      <div className={Styles.content}>
+        <div className={Styles.item}>
+            <p className={Styles.title}>FullStack Developer</p>
+            <p className={Styles.subTitle}>Company Name</p>
+            <p className={Styles.date}>
+                <Calendar/> 12/08/2022 - 2/02/2023
+            </p>
+            <a className={Styles.link}>
+                <Paperclip/>
+                htttps://site.com
+            </a>
+            <p className={Styles.location}>
+                <MapPin/>
+                USA
+                </p>
         </div>
-        <div className={Styles.main}>
-            <div className={Styles.col1}>
-                {column[0]}
-            </div>
-            <div className={Styles.col2}>
-                {column[1]}
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
+
+  const projectsSection = (
+    <div key={"projects"} className={`${Styles.section} ${Styles.projects}`}>
+      <div className={Styles.sectionTitle}>Projects</div>
+      <div className={Styles.content}>
+        <div className={Styles.item}>
+            <p className={Styles.title}>Project 1</p>
+            <a className={Styles.link}>
+                <Paperclip/>
+                htttps://site.com
+            </a>
+            <a className={Styles.link}>
+                <GitHub/>
+                htttps://github.com
+            </a>
+            <p className={Styles.overview}>This is not a real project it is a dummy project made to test the sekeleton</p>
+            <ul className={Styles.points}>
+                <li className={Styles.point}>This is first point</li>
+                <li className={Styles.point}>This is first point</li>
+                <li className={Styles.point}>This is first point</li>
+                <li className={Styles.point}>This is first point</li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  );
+
+  const educationSection = (
+    <div key={"education"} className={`${Styles.section} ${Styles.education}`}>
+      <div className={Styles.sectionTitle}>Education</div>
+      <div className={Styles.content}>
+        <div className={Styles.item}>
+            <p className={Styles.title}>B-tech</p>
+            <p className={Styles.subTitle}>Collage Name</p>
+            <p className={Styles.date}>
+                <Calendar/> 12/08/2022 - 2/02/2023
+            </p>
+            </div>
+    </div>
+    </div>
+  );
+
+  const achievmentsSection = (
+    <div
+      key={"achievments"}
+      className={`${Styles.section} ${Styles.achievments}`}
+    >
+      <div className={Styles.sectionTitle}>Achievments</div>
+      <div className={Styles.numbered}>
+      <ul className={Styles.points}>
+                <li className={Styles.point}>This is first point</li>
+                <li className={Styles.point}>This is first point</li>
+                <li className={Styles.point}>This is first point</li>
+                <li className={Styles.point}>This is first point</li>
+            </ul>
+      </div>
+    </div>
+  );
+
+  const otherSection = (
+    <div key={"other"} className={`${Styles.section} ${Styles.other}`}>
+      <div className={Styles.sectionTitle}>Other</div>
+      <div className={Styles.content}>
+        <p className={Styles.overview}>This is some dummy text</p>
+      </div>
+    </div>
+  );
+
+  const summarySection = (
+    <div key={"summary"} className={`${Styles.section} ${Styles.summary}`}>
+      <div className={Styles.sectionTitle}>Summary</div>
+      <div className={Styles.content}>
+        <p className={Styles.overview}>This is some dummy text</p>
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    setColumn([
+      [projectsSection, educationSection, summarySection],
+
+      [workExpSection, achievmentsSection, otherSection],
+    ]);
+  }, []);
+
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.header}>
+        <p className={Styles.heading}>Name</p>
+        <p className={Styles.subHeading}>Blockchain Developer</p>
+
+        <div className={Styles.links}>
+          <a className={Styles.link}>
+            <AtSign />
+            abc@gmail.com
+          </a>
+          <a className={Styles.link}>
+            <Linkedin />
+            linkedin.com
+          </a>
+          <a className={Styles.link}>
+            <GitHub />
+            github.com
+          </a>
+
+          <a className={Styles.link}>
+            <Phone />
+            1234567890
+          </a>
+        </div>
+      </div>
+      <div className={Styles.main}>
+        <div className={Styles.col1}>{column[0]}</div>
+        <div className={Styles.col2}>{column[1]}</div>
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
