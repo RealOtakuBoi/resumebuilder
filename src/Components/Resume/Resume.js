d1cf42fdc008de2f728d0a84f33cee5e48f641d8
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-31 20:29:05 +0530

    some bugs waiting fixing

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -9,103 +9,224 @@ import { Phone } from "react-feather";
 import { Paperclip } from "react-feather";
 
 function Resume(props) {
+    const information = props.information;
+    const sections = props.sections;
   const [column, setColumn] = useState([[], []]);
+
+  const info = {
+    workExp: information[sections.workExp],
+    projects: information[sections.projects],
+    achievments: information[sections.achievement],
+    education: information[sections.education],
+    basicInfo: information[sections.basicInfo],
+    summary: information[sections.summary],
+    other: information[sections.other],
+  };
+
+  const getFormattedDate = (value) =>{
+    if(!value) return ''
+    const date = new Date(value)
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  }
 
   const workExpSection = (
     <div key={"workExp"} className={`${Styles.section} ${Styles.workExp}`}>
-      <div className={Styles.sectionTitle}>Work Experience</div>
+      <div className={Styles.sectionTitle}>{info.workExp?.sectionTitle}</div>
       <div className={Styles.content}>
-        <div className={Styles.item}>
-            <p className={Styles.title}>FullStack Developer</p>
-            <p className={Styles.subTitle}>Company Name</p>
-            <p className={Styles.date}>
-                <Calendar/> 12/08/2022 - 2/02/2023
-            </p>
-            <a className={Styles.link}>
+        {
+            info.workExp?.details?.map((item) => (
+                <div className={Styles.item} key={item.title}>
+                {
+                    item.title &&
+                <p className={Styles.title}>{item.title}</p>
+                }
+
+                {
+                    item.companyName &&
+            <p className={Styles.subTitle}>{item.companyName}</p>
+                }
+
+                {
+                    item.certificationLink && 
+            <a className={Styles.link} href ={item.certificationLink}>
                 <Paperclip/>
-                htttps://site.com
+                {item.certificationLink}
             </a>
+                }
+
+                {
+                item.startDate && item.endDate ? (
+                    <div className={Styles.date}>
+                      <Calendar /> {getFormattedDate(item.startDate)}-
+                      {getFormattedDate(item.endDate)}
+                    </div>   ) : (
+                        ''
+                    )}
+                
+
+                {
+                    item.location && 
             <p className={Styles.location}>
                 <MapPin/>
-                USA
+                {item.location}
                 </p>
+                }
+
+                {
+                    item.points?.length > 0 && 
+                    <ul className={Styles.points}>
+                        {
+                            item.points?.map((elem, index) => (
+                                <li className={Styles.point} key={elem+index}>
+                                    {elem}
+                                    </li>
+                            ))
+                        }
+                    </ul>
+                }
+                    
+                
+
         </div>
+            ))
+        }
+        
       </div>
     </div>
   );
 
   const projectsSection = (
     <div key={"projects"} className={`${Styles.section} ${Styles.projects}`}>
-      <div className={Styles.sectionTitle}>Projects</div>
+      <div className={Styles.sectionTitle}>{info.projects?.sectionTitle}</div>
       <div className={Styles.content}>
-        <div className={Styles.item}>
-            <p className={Styles.title}>Project 1</p>
-            <a className={Styles.link}>
+        {
+            info.projects?.details?.map((item) => (
+                <div className={Styles.item}>
+                    {
+                        item.title &&
+            <p className={Styles.title}>{item.title}</p>
+                    }
+
+                    {
+                        item.link && 
+            <a className={Styles.link} href={item.link}>
                 <Paperclip/>
-                htttps://site.com
+                {item.link}
             </a>
-            <a className={Styles.link}>
+                    }
+
+                    {
+                        item.github && 
+            <a className={Styles.link} href ={item.github}>
                 <GitHub/>
-                htttps://github.com
+                {item.github}
             </a>
-            <p className={Styles.overview}>This is not a real project it is a dummy project made to test the sekeleton</p>
-            <ul className={Styles.points}>
-                <li className={Styles.point}>This is first point</li>
-                <li className={Styles.point}>This is first point</li>
-                <li className={Styles.point}>This is first point</li>
-                <li className={Styles.point}>This is first point</li>
-            </ul>
+                    }
+
+                    {
+                        item.overview &&
+            <p className={Styles.overview}>{item.overview}</p>
+                    }
+
+
+
+{
+                    item.points?.length > 0 && 
+                    <ul className={Styles.points}>
+                        {
+                            item.points?.map((elem, index) => (
+                                <li className={Styles.point} key={elem+index}>
+                                    {elem}
+                                    </li>
+                            ))
+                        }
+                    </ul>
+                }
         </div>
+            ))
+        }
+        
       </div>
     </div>
   );
 
   const educationSection = (
     <div key={"education"} className={`${Styles.section} ${Styles.education}`}>
-      <div className={Styles.sectionTitle}>Education</div>
+      <div className={Styles.sectionTitle}>{info.education?.sectionTitle}</div>
       <div className={Styles.content}>
-        <div className={Styles.item}>
-            <p className={Styles.title}>B-tech</p>
-            <p className={Styles.subTitle}>Collage Name</p>
-            <p className={Styles.date}>
-                <Calendar/> 12/08/2022 - 2/02/2023
-            </p>
+        {
+            info.education?.details?.map((item) => (
+                <div className={Styles.item}>
+                    {
+                        item.title &&
+            <p className={Styles.title}>{item.title}</p>
+                    }
+
+                    {
+                        item.college &&
+            <p className={Styles.subTitle}>{item.college}</p>
+                    }
+
+                    {
+
+                    }
+
+
+                    {
+                item.startDate && item.endDate ? (
+                    <div className={Styles.date}>
+                      <Calendar /> {getFormattedDate(item.startDate)}-
+                      {getFormattedDate(item.endDate)}
+                    </div>   ) : (
+                        ''
+                    )}
             </div>
+            ))
+        }
+        
     </div>
     </div>
   );
 
   const achievmentsSection = (
     <div
-      key={"achievments"}
-      className={`${Styles.section} ${Styles.achievments}`}
-    >
-      <div className={Styles.sectionTitle}>Achievments</div>
-      <div className={Styles.numbered}>
-      <ul className={Styles.points}>
-                <li className={Styles.point}>This is first point</li>
-                <li className={Styles.point}>This is first point</li>
-                <li className={Styles.point}>This is first point</li>
-                <li className={Styles.point}>This is first point</li>
-            </ul>
+        key={"achievement"}
+        className={`${Styles.section} ${Styles.achievments}`}
+      >
+         <div className={Styles.sectionTitle}>
+          Achievments
+        </div>
+        <div className={Styles.content}>
+        {
+                    info.achievments?.points?.length > 0 && 
+                    <ul className={Styles.numbered}>
+                        {
+                            info.achievments?.points?.map((elem, index) => (
+                                <li className={Styles.points} key={elem+index}>
+                                    {elem}
+                                    </li>
+                            ))
+                        }
+                    </ul>
+                }
+        </div>
       </div>
-    </div>
-  );
+  )
 
   const otherSection = (
     <div key={"other"} className={`${Styles.section} ${Styles.other}`}>
-      <div className={Styles.sectionTitle}>Other</div>
+      <div className={Styles.sectionTitle}>{info.other?.sectionTitle}</div>
       <div className={Styles.content}>
-        <p className={Styles.overview}>This is some dummy text</p>
+        <p className={Styles.overview}>{info.other?.detail}</p>
       </div>
     </div>
   );
 
   const summarySection = (
     <div key={"summary"} className={`${Styles.section} ${Styles.summary}`}>
-      <div className={Styles.sectionTitle}>Summary</div>
+      <div className={Styles.sectionTitle}>{info.summary?.sectionTitle}</div>
       <div className={Styles.content}>
-        <p className={Styles.overview}>This is some dummy text</p>
+        <p className={Styles.overview}>{info.summary?.summary}</p>
       </div>
     </div>
   );
@@ -117,6 +238,11 @@ function Resume(props) {
       [workExpSection, achievmentsSection, otherSection],
     ]);
   }, []);
+
+
+  useEffect(() => {
+
+  },[information])
 
   return (
     <div className={Styles.container}>
