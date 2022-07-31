d3bb6c3447ee53127230baf376ed94fa0a91d2de
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-31 16:19:05 +0530

    making resume skeleton

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -4,10 +4,35 @@ import React from 'react'
 
 import Styles from './Resume.module.css'
 
+import { GitHub } from 'react-feather'
+import { Linkedin } from 'react-feather'
+import { AtSign } from 'react-feather'
+import { Phone } from 'react-feather'
+
 function Resume() {
   return (
     <div className={Styles.container}>
-        
+        <div className={Styles.header}>
+            <p className={Styles.heading}>Name</p>
+            <p className={Styles.subHeading}>Blockchain Developer</p>
+
+            <div className={Styles.links}>
+                <div className={Styles.link}>
+                    <AtSign/>
+                    abc@gmail.com</div>
+                <div className={Styles.link}>
+                    <Linkedin/>
+                    linkedin.com</div>
+                <div className={Styles.link}>
+                    <GitHub/>
+                    github.com</div>
+
+                <div className={Styles.link}>
+                    <Phone/>
+                    1234567890</div>
+
+            </div>
+        </div>
     </div>
   )
 }
