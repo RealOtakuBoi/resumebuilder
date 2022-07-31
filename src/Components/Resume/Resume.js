f87a7bf0d52a35120a67512066ef642013d93b52
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-31 17:48:11 +0530

    some more styling

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Styles from './Resume.module.css'
 
@@ -11,29 +11,44 @@ import { Phone } from 'react-feather'
 
 function Resume(props) {
 
-    const workExpSection = (<div className={`${Styles.section} ${Styles.workExp}`}>
+    const [column, setColumn] = useState([[],[]]);
+
+    const workExpSection = (<div key={'workExp'} className={`${Styles.section} ${Styles.workExp}`}>
         <div className={Styles.sectionTitle}>Work Experience</div>
     </div>)
 
-const projectsSection = (<div className={`${Styles.section} ${Styles.projects}`}>
+const projectsSection = (<div key={'projects'} className={`${Styles.section} ${Styles.projects}`}>
 <div className={Styles.sectionTitle}>Projects</div>
 </div>)
 
-const educationSection = (<div className={`${Styles.section} ${Styles.education}`}>
+const educationSection = (<div  key={'education'} className={`${Styles.section} ${Styles.education}`}>
         <div className={Styles.sectionTitle}>Education</div>
     </div>)
 
-const achievmentsSection = (<div className={`${Styles.section} ${Styles.achievments}`}>
+const achievmentsSection = (<div key={'achievments'} className={`${Styles.section} ${Styles.achievments}`}>
 <div className={Styles.sectionTitle}>Achievments</div>
 </div>)
 
-const otherSection = (<div className={`${Styles.section} ${Styles.other}`}>
+const otherSection = (<div key={'other'} className={`${Styles.section} ${Styles.other}`}>
 <div className={Styles.sectionTitle}>Other</div>
 </div>)
 
-const summarySection = (<div className={`${Styles.section} ${Styles.summary}`}>
+const summarySection = (<div  key={'summary'} className={`${Styles.section} ${Styles.summary}`}>
 <div className={Styles.sectionTitle}>Summary</div>
 </div>)
+
+
+    useEffect(() => {
+        setColumn([
+            [
+                projectsSection, educationSection, summarySection
+            ],
+
+            [
+                workExpSection, achievmentsSection, otherSection
+            ]
+        ])
+    },[])
 
     
   return (
@@ -44,20 +59,28 @@ const summarySection = (<div className={`${Styles.section} ${Styles.summary}`}>
             <p className={Styles.subHeading}>Blockchain Developer</p>
 
             <div className={Styles.links}>
-                <div className={Styles.link}>
+                <a className={Styles.link}>
                     <AtSign/>
-                    abc@gmail.com</div>
-                <div className={Styles.link}>
+                    abc@gmail.com</a>
+                <a className={Styles.link}>
                     <Linkedin/>
-                    linkedin.com</div>
-                <div className={Styles.link}>
+                    linkedin.com</a>
+                <a className={Styles.link}>
                     <GitHub/>
-                    github.com</div>
+                    github.com</a>
 
-                <div className={Styles.link}>
+                <a className={Styles.link}>
                     <Phone/>
-                    1234567890</div>
+                    1234567890</a>
 
+            </div>
+        </div>
+        <div className={Styles.main}>
+            <div className={Styles.col1}>
+                {column[0]}
+            </div>
+            <div className={Styles.col2}>
+                {column[1]}
             </div>
         </div>
     </div>
