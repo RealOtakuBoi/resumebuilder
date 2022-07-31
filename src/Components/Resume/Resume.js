3b90da34d0caf365cd9ba31387a99bc69abaff2f
RealOtakuBoi <sohamseth2005@gmail.com>
2022-07-31 20:39:51 +0530

    big edit

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -29,6 +29,206 @@ function Resume(props) {
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
   }
 
+  const sectioDiv = {
+    [sections.workExp]:
+    <div key={"workExp"} className={`${Styles.section} ${Styles.workExp}`}>
+    <div className={Styles.sectionTitle}>{info.workExp?.sectionTitle}</div>
+    <div className={Styles.content}>
+      {
+          info.workExp?.details?.map((item) => (
+              <div className={Styles.item} key={item.title}>
+              {
+                  item.title &&
+              <p className={Styles.title}>{item.title}</p>
+              }
+
+              {
+                  item.companyName &&
+          <p className={Styles.subTitle}>{item.companyName}</p>
+              }
+
+              {
+                  item.certificationLink && 
+          <a className={Styles.link} href ={item.certificationLink}>
+              <Paperclip/>
+              {item.certificationLink}
+          </a>
+              }
+
+              {
+              item.startDate && item.endDate ? (
+                  <div className={Styles.date}>
+                    <Calendar /> {getFormattedDate(item.startDate)}-
+                    {getFormattedDate(item.endDate)}
+                  </div>   ) : (
+                      ''
+                  )}
+              
+
+              {
+                  item.location && 
+          <p className={Styles.location}>
+              <MapPin/>
+              {item.location}
+              </p>
+              }
+
+              {
+                  item.points?.length > 0 && 
+                  <ul className={Styles.points}>
+                      {
+                          item.points?.map((elem, index) => (
+                              <li className={Styles.point} key={elem+index}>
+                                  {elem}
+                                  </li>
+                          ))
+                      }
+                  </ul>
+              }
+                  
+              
+
+      </div>
+          ))
+      }
+      
+    </div>
+  </div>,
+
+    [sections.projects]:
+    <div key={"projects"} className={`${Styles.section} ${Styles.projects}`}>
+    <div className={Styles.sectionTitle}>{info.projects?.sectionTitle}</div>
+    <div className={Styles.content}>
+      {
+          info.projects?.details?.map((item) => (
+              <div className={Styles.item}>
+                  {
+                      item.title &&
+          <p className={Styles.title}>{item.title}</p>
+                  }
+
+                  {
+                      item.link && 
+          <a className={Styles.link} href={item.link}>
+              <Paperclip/>
+              {item.link}
+          </a>
+                  }
+
+                  {
+                      item.github && 
+          <a className={Styles.link} href ={item.github}>
+              <GitHub/>
+              {item.github}
+          </a>
+                  }
+
+                  {
+                      item.overview &&
+          <p className={Styles.overview}>{item.overview}</p>
+                  }
+
+
+
+{
+                  item.points?.length > 0 && 
+                  <ul className={Styles.points}>
+                      {
+                          item.points?.map((elem, index) => (
+                              <li className={Styles.point} key={elem+index}>
+                                  {elem}
+                                  </li>
+                          ))
+                      }
+                  </ul>
+              }
+      </div>
+          ))
+      }
+      
+    </div>
+  </div>,
+
+    [sections.education]:
+    <div key={"education"} className={`${Styles.section} ${Styles.education}`}>
+      <div className={Styles.sectionTitle}>{info.education?.sectionTitle}</div>
+      <div className={Styles.content}>
+        {
+            info.education?.details?.map((item) => (
+                <div className={Styles.item}>
+                    {
+                        item.title &&
+            <p className={Styles.title}>{item.title}</p>
+                    }
+
+                    {
+                        item.college &&
+            <p className={Styles.subTitle}>{item.college}</p>
+                    }
+
+                    {
+
+                    }
+
+
+                    {
+                item.startDate && item.endDate ? (
+                    <div className={Styles.date}>
+                      <Calendar /> {getFormattedDate(item.startDate)}-
+                      {getFormattedDate(item.endDate)}
+                    </div>   ) : (
+                        ''
+                    )}
+            </div>
+            ))
+        }
+        
+    </div>
+    </div>,
+
+    [sections.achievments]:
+    <div
+        key={"achievement"}
+        className={`${Styles.section} ${Styles.achievments}`}
+      >
+         <div className={Styles.sectionTitle}>
+          Achievments
+        </div>
+        <div className={Styles.content}>
+        {
+                    info.achievments?.points?.length > 0 && 
+                    <ul className={Styles.numbered}>
+                        {
+                            info.achievments?.points?.map((elem, index) => (
+                                <li className={Styles.points} key={elem+index}>
+                                    {elem}
+                                    </li>
+                            ))
+                        }
+                    </ul>
+                }
+        </div>
+      </div>,
+
+    [sections.summary]:
+    <div key={"summary"} className={`${Styles.section} ${Styles.summary}`}>
+      <div className={Styles.sectionTitle}>{info.summary?.sectionTitle}</div>
+      <div className={Styles.content}>
+        <p className={Styles.overview}>{info.summary?.summary}</p>
+      </div>
+    </div>,
+
+    [sections.other]:
+    <div key={"other"} className={`${Styles.section} ${Styles.other}`}>
+      <div className={Styles.sectionTitle}>{info.other?.sectionTitle}</div>
+      <div className={Styles.content}>
+        <p className={Styles.overview}>{info.other?.detail}</p>
+      </div>
+    </div>
+
+
+  }
+
   const workExpSection = (
     <div key={"workExp"} className={`${Styles.section} ${Styles.workExp}`}>
       <div className={Styles.sectionTitle}>{info.workExp?.sectionTitle}</div>
@@ -233,9 +433,9 @@ function Resume(props) {
 
   useEffect(() => {
     setColumn([
-      [projectsSection, educationSection, summarySection],
+      [sections.projects, sections.education, sections.summary],
 
-      [workExpSection, achievmentsSection, otherSection],
+      [sections.workExp,sections.achievments ,sections.other ],
     ]);
   }, []);
 
